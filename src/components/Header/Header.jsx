6c1f3a0a525b0ba117f1047c27916f272599b3ed
Andriy_Chornobay <andriy.chornobay@gmail.com>
2025-09-17 18:02:26 +0300

    add initial files

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import css from "./Header.module.css";
-
+import { useState } from "react";
 import SearchIcon from "../../assets/icons/search.svg";
 import UserIcon from "../../assets/icons/user.svg";
 import BagIcon from "../../assets/icons/shoppingBag.svg";
 import { GiHamburgerMenu } from "react-icons/gi";
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <header className={css.header}>
       <div className={css.container}>
@@ -26,9 +27,18 @@ export default function Header() {
           <button>
             <img src={UserIcon} alt="User" className={css.icon} />
           </button>
-          <button className={css.burgerIcon}>
-            <GiHamburgerMenu />
+          <button className={css.burgerIcon} onClick={() => setIsOpen(!isOpen)}>
+            {isOpen ? "✕" : <GiHamburgerMenu />}
           </button>
+          {isOpen && (
+            <div className={css.mobileNav}>
+              <a href="#">Home</a>
+              <a href="#">About</a>
+              <a href="#">Reviews</a>
+              <a href="#">Contact</a>
+              <a href="#">Shop</a>
+            </div>
+          )}
         </div>
       </div>
     </header>
